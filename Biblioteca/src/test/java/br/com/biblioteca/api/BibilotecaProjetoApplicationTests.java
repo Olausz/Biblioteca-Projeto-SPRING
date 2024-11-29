79a287d6999f3e908510ef73ff3cc4f963acfa74
Saulo Pinto <142450509+Olausz@users.noreply.github.com>
2024-11-29 11:26:56 -0300

    Add files via upload

---
+++ Biblioteca/src/test/java/br/com/biblioteca/api/BibilotecaProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.biblioteca.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibilotecaProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
